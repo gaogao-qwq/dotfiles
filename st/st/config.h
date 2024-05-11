d79232ec5a763f05acf871f7bb2b6e97bb5c06e1
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-11 11:18:58 +0800

    Chore: Update st font
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- st/st/config.h
+++ st/st/config.h
@@ -5,10 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char *font = "Maple Mono:size=12:antialias=true:autohint=true";
 static char *font2[] = {
 	"WenQuanYi Micro Hei Mono:size=12:antialias=true:autohint=true",
-	"Inconsolata for Powerline:size=12:antialias=true:autohint=true"
 };
 static int borderpx = 0;
 
